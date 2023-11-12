2dae68b803dca120f98b797ea4a5d8505a79f621
Austinraj <malaraustinraja25@gmail.com>
2023-11-12 13:05:46 +0530

    commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// npm - node package manager
+// npm init
+// or  npm init -y -> no qustion
+
+
+// npm install react
+// npm install eslint --save-dev   ->devDependencies install
+
+
+// npm install -> dependencies and devDependencies install panum
+// npm install --production -> dependencies only install pannu
+
+// npm uninstall react
+
+
+
+// scripts :{
+//     "start":""
+// }
+
+// npm start
+
+
+// NPM => package-lock.json  => github wbsite npm
+// Yarn => yarn.lock =>  facebooke website Yarn
+// yarn install
+// yarn add react
+// yarn add eslint --dev
+// yarn open scoure website
+
+
+// https://www.npmjs.com/
+// https://create-react-app.dev/docs/getting-started
+
+
+
+// yarn create react-app austin-app
