3c054f19d9965302f41b3463e304f68fbe254948
Austinraj <malaraustinraja25@gmail.com>
2023-11-26 11:44:03 +0530

    commit

---
+++ austin-app/src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Banner() {
+    return (
+        <div className="carousel-inner">
+            <div className="carousel-item active">
+                <a href="">
+                    <img src="/images/banner.webp" className="b-block w-100" alt="Guvi banner" />
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
--- austin-app/src/containers/Home.js
+++ austin-app/src/containers/Home.js
@@ -1,12 +1,32 @@
 import React from "react";
+
+import Banner from "../components/Banner";
 function Home({ history, location}){
     //goBack
     // goforward
     // push
     // replace
     
+    const [count, setCount] = React.useState(0);
 
-    // console.log(location);
+    const countRef = React.useRef(); // useRef Last decaler panna 2 CountRef use panna last iruka use pannum    // console.log(location);
+
+    const inc = () => setCount(count + 1);
+    const dec = () => setCount(count - 1);
+    const res = () => setCount(0);
+
+    const incRef = () =>{
+         countRef.current.innerText = Number(countRef.current.innerText) +1;
+    }
+    const decRef = () =>{
+         countRef.current.innerText = Number(countRef.current.innerText)  - 1;
+
+    }
+    const resRef = () =>{
+        countRef.current.innerText = Number(countRef.current.innerText = 0);
+         
+    }
+
     return(
        <div className="carousel slide" data-be-ride="carousel">
         <br />
@@ -16,13 +36,24 @@ function Home({ history, location}){
         <button className="btn btn-danger" onClick={() => history.replace("/about")}>Go About Replace</button> 
         {/* history.replace  bank payment use pannlam */}
         <br /> <br />
-        <div className="carousel-inner">
-        <div className="carousel-item active">
-        <a href="">
-            <img src="/images/banner.webp" className="b-block w-100" alt="Guvi banner" />
-        </a>
-        </div>
-        </div>
+
+        {/* useSate */}
+        <h2>Counter - {count}</h2>
+        <button className="btn btn-primary me-2" onClick={inc}>+</button>
+        <button className="btn btn-warning me-2" onClick={dec}>-</button>
+        <button className="btn btn-danger me-2" onClick={res}>0</button>
+        <br />
+        <br />
+        {/* use Ref */}
+        <h2>Counter - <span ref={countRef}>0</span></h2>
+        <button className="btn btn-primary me-2" onClick={incRef}>+</button>
+        <button className="btn btn-warning me-2" onClick={decRef}>-</button>
+        <button className="btn btn-danger me-2" onClick={resRef}>0</button>
+
+        <br />
+        <br />
+        <button className="btn btn-warning">See Banner</button>
+        <Banner />
        </div>
     )
 }
